96bb3921638fec46188bf2ed1b4e2c4b7acaf292
Gustavo <gust.bersan@gmail.com>
2022-07-05 17:41:01 -0300

    Resolvendo terceiro exercício
    Proposta do exercício comentado no código

--- Csharp/Exercicios/Program.cs
+++ Csharp/Exercicios/Program.cs
@@ -7,7 +7,7 @@ namespace Exercicios
         static void Main(string[] args)
         {
             Console.Clear();
-            SegundoTeste();
+            TerceiroTeste();
         }
         static void PrimeiroTeste()
         //Fazer um programa para ler o código de uma peça (peça 1), o número de peças 1, o valor unitário de cada peça 1,
@@ -42,6 +42,66 @@ namespace Exercicios
             area = pi * (raio * raio);
 
             Console.WriteLine("A=" + area.ToString("N4"));
+        }
+        static void TerceiroTeste()
+        // Com base em uma tabela de preços, faça um programa que leia o código de um item e a quantidade deste item.
+        // A seguir, calcule e mostre o valor da conta a pagar.
+        {
+            double hotDog, xSalada, xBacon, torradaSimples, refrigerante, total;
+
+            Console.WriteLine("Tabela de pedidos!!!");
+            Console.WriteLine("\n1 - Cachorro-quente   R$4,00");
+            Console.WriteLine("2 - X-Salada   R$4,50");
+            Console.WriteLine("3 - X-Bacon   R$5,00");
+            Console.WriteLine("4 - Torrada Simples   R$2,00");
+            Console.WriteLine("5 - Refrigerante   R$1,50\n");
+
+            Console.WriteLine("Informe o número do pedido");
+            int numPedido = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe a quantidade de pedido");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            switch (numPedido)
+            {
+                case 1:
+                    {
+                        hotDog = 4.00;
+                        total = hotDog * quantidade;
+                        Console.WriteLine($"Total: R$ {total.ToString("N2")}");
+                        break;
+                    }
+                case 2:
+                    {
+                        xSalada = 4.50;
+                        total = xSalada * quantidade;
+                        Console.WriteLine($"Total: R$ {total.ToString("N2")}");
+                        break;
+                    }
+                case 3:
+                    {
+                        xBacon = 5.00;
+                        total = xBacon * quantidade;
+                        Console.WriteLine($"Total: R$ {total.ToString("N2")}");
+                        break;
+                    }
+                case 4:
+                    {
+                        torradaSimples = 2.00;
+                        total = torradaSimples * quantidade;
+                        Console.WriteLine($"Total: R$ {total.ToString("N2")}");
+                        break;
+                    }
+                case 5:
+                    {
+                        refrigerante = 1.50;
+                        total = refrigerante * quantidade;
+                        Console.WriteLine($"Total: R$ {total.ToString("N2")}");
+                        break;
+                    }
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
         }
     }
 }
