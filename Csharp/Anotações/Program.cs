b50c56d49e7106ed5d4ae6071f809f680354d747
Gustavo <gust.bersan@gmail.com>
2022-07-14 22:40:11 -0300

    Criando projeto de anotação
    Anotando primeiras instruções e funções de uma aplicação C#.

---
+++ Csharp/Anotações/Program.cs
@@ -0,0 +1,12 @@
+﻿using System; //Importações -> Sempre definidas pelo "using", serve para definir quais bibliotecas/namespaces de código o programa irá usar.
+
+namespace Anotações //Divisões lógicas -> Normalmente acompanham o nome da pasta em que está. É um agrupamento de classes relacionadas.
+{
+    class Program //Unidade base da programação orientada a objetos (POO). Todo o código a ser escrito em C#, deve estar dentro de uma classe.
+    {
+        static void Main(string[] args) //Declaração padrão do C# para denominar o ponto de entrada da aplicação (aonde a execução começa).
+        {
+            Console.WriteLine("Hello World!"); //Comando de saída de texto. Imprime/exibe o texto na tela do usuário.
+        }
+    }
+}
