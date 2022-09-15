d599136c8d7280bd53c2b011ffa06da1627e77f3
Gustavo <gust.bersan@gmail.com>
2022-09-15 18:52:29 -0300

    Solucionando sétimo exercício sobre estruturas condicionais

--- Csharp/Exercicios/FixExercises.cs
+++ Csharp/Exercicios/FixExercises.cs
@@ -223,5 +223,29 @@ namespace ConditionalStructures
             else
                 Console.WriteLine("Fora de Intervalo");
         }
+
+        public static void ExeProp7()
+        // Leia 2 valores com uma casa decimal (x e y), que devem representar as coordenadas
+        // de um ponto em um plano. A seguir, determine qual o quadrante ao qual pertence o
+        // ponto, ou se está sobre um dos eixos cartesianos ou na origem (x = y = 0).
+        // Se o ponto estiver na origem, escreva a mensagem “Origem”.
+        // Se o ponto  estiver sobre um dos eixos escreva “Eixo X” ou “Eixo Y”, conforme for a
+        // situação.
+        {
+            string[] values = Console.ReadLine().Split(' ');
+            double x = double.Parse(values[0]);
+            double y = double.Parse(values[1]);
+
+            if (x == 0 && y == 0)
+                Console.WriteLine("Origem");
+            else if (x > 0 && y > 0)
+                Console.WriteLine("Q1");
+            else if (x > 0 && y < 0)
+                Console.WriteLine("Q4");
+            else if (x < 0 && y > 0)
+                Console.WriteLine("Q2");
+            else
+                Console.WriteLine("Q3");
+        }
     }
 }
