f72d9c3cb44633d91faa37c6a8cbbeff89184ba0
Gustavo <gust.bersan@gmail.com>
2022-09-15 18:53:05 -0300

    Realizando teste no sétimo exercício proposto

--- Csharp/Exercicios/Program.cs
+++ Csharp/Exercicios/Program.cs
@@ -12,7 +12,7 @@ namespace Exercicios
         public static void Main(string[] args)
         {
             Console.Clear();
-            CondtStructure.ExeProp6();
+            CondtStructure.ExeProp7();
         }
         static void PrimeiroTeste()
         //Fazer um programa para ler o código de uma peça (peça 1), o número de peças 1, o valor unitário de cada peça 1,
