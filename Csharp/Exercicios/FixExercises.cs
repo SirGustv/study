2a40c034aaed837bbe444e6c029e05f10300f327
Gustavo <gust.bersan@gmail.com>
2022-09-15 18:37:18 -0300

    Solucionando sexto exercício sobre estruturas condicionais

--- Csharp/Exercicios/FixExercises.cs
+++ Csharp/Exercicios/FixExercises.cs
@@ -204,7 +204,24 @@ namespace ConditionalStructures
                 price = 1.50 * qntd;
 
             Console.WriteLine("Total: R$ " + price.ToString("F2"));
+        }
+        public static void ExeProp6()
+        // Você deve fazer um programa que leia um valor qualquer e apresente uma mensagem dizendo em qual dos
+        // seguintes intervalos ([0,25], (25,50], (50,75], (75,100]) este valor se encontra. Obviamente se o valor não estiver em
+        // nenhum destes intervalos, deverá ser impressa a mensagem “Fora de intervalo”.
+        {
+            double value = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
+            if (value >= 0.0 && value <= 25.0)
+                Console.WriteLine("Intervalo (0,25)");
+            else if (value > 25.0 && value <= 50.0)
+                Console.WriteLine("Intervalo (25,50)");
+            else if (value > 50.0 && value <= 75.0)
+                Console.WriteLine("Intervalo (50,75)");
+            else if (value > 75.0 && value <= 100.0)
+                Console.WriteLine("Intervalo (75,100)");
+            else
+                Console.WriteLine("Fora de Intervalo");
         }
     }
 }
