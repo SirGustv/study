0ca02451cbe4943ab5a279e6fe310ac6db500908
Gustavo <gust.bersan@gmail.com>
2022-09-21 17:34:48 -0300

    Solucionando Exercício
    Tercieiro exercício proposto sobre a estrutura 'For'.

--- Csharp/Exercicios/FixExercises.cs
+++ Csharp/Exercicios/FixExercises.cs
@@ -396,5 +396,25 @@ namespace ForStructure
             Console.WriteLine($"{inside} in");
             Console.WriteLine($"{outside} out");
         }
+        public static void ExeProp3()
+        // Leia 1 valor inteiro N, que representa o número de casos de teste que vem a seguir. Cada caso de teste consiste
+        // de 3 valores reais, cada um deles com uma casa decimal. Apresente a média ponderada para cada um destes
+        // conjuntos de 3 valores, sendo que o primeiro valor tem peso 2, o segundo valor tem peso 3 e o terceiro valor tem
+        // peso 5.
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                string[] values = Console.ReadLine().Split(' ');
+                double a = double.Parse(values[0], CultureInfo.InvariantCulture);
+                double b = double.Parse(values[1], CultureInfo.InvariantCulture);
+                double c = double.Parse(values[2], CultureInfo.InvariantCulture);
+
+                double media = (a * 2.0 + b * 3.0 + c * 5.0) / 10;
+
+                Console.WriteLine(media.ToString("F1", CultureInfo.InvariantCulture));
+            }
+        }
     }
 }
