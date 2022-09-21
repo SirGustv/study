1738316e7080e1b7eab82dfe74666d7951996d5a
Gustavo <gust.bersan@gmail.com>
2022-09-21 12:08:39 -0300

    Iniciando exercícios sobre a estrutura 'While'
    Este bloco de exercícios contará com três exercícios propostos, O primeiro exercício já foi solucionado neste commit. A descrição do exercício está comentada no código.

--- Csharp/Exercicios/FixExercises.cs
+++ Csharp/Exercicios/FixExercises.cs
@@ -283,3 +283,23 @@ namespace ConditionalStructures
         }
     }
 }
+
+namespace WhileStructure
+{
+    public class WhileStruct
+    {
+        public static void ExeProp1()
+        // Escreva um programa que repita a leitura de uma senha até que ela seja válida. Para cada leitura de senha
+        // incorreta informada, escrever a mensagem "Senha Invalida". Quando a senha for informada corretamente deve ser
+        // impressa a mensagem "Acesso Permitido" e o algoritmo encerrado. Considere que a senha correta é o valor 2002.
+        {
+            int password = int.Parse(Console.ReadLine());
+            while (password != 2002)
+            {
+                Console.WriteLine("Senha Inválida");
+                password = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Acesso Permitido");
+        }
+    }
+}
