68368cdfad2550c5109ee47bd516d34edf1298c0
Gustavo <gust.bersan@gmail.com>
2022-09-21 14:10:23 -0300

    Solucionando terceiro exercício proposto sobre a estrutura 'While'

--- Csharp/Exercicios/FixExercises.cs
+++ Csharp/Exercicios/FixExercises.cs
@@ -325,5 +325,36 @@ namespace WhileStructure
                 y = int.Parse(entry[1]);
             }
         }
+        public static void ExeProp3()
+        // Um Posto de combustíveis deseja determinar qual de seus produtos tem a preferência de seus clientes. Escreva
+        // um algoritmo para ler o tipo de combustível abastecido (codificado da seguinte forma: 1.Álcool 2.Gasolina 3.Diesel
+        // 4.Fim). Caso o usuário informe um código inválido (fora da faixa de 1 a 4) deve ser solicitado um novo código (até
+        // que seja válido). O programa será encerrado quando o código informado for o número 4. Deve ser escrito a
+        // mensagem: "MUITO OBRIGADO" e a quantidade de clientes que abasteceram cada tipo de combustível, conforme
+        // exemplo.
+        {
+            int alcool = 0;
+            int gasoline = 0;
+            int diesel = 0;
+
+            int product = int.Parse(Console.ReadLine());
+
+            while (product != 4)
+            {
+                if (product == 1)
+                    alcool = alcool + 1;
+                else if (product == 2)
+                    gasoline = gasoline + 1;
+                else if (product == 3)
+                    diesel = diesel + 1;
+                else
+                    Console.WriteLine("Número invválido, digite um número válido");
+
+                product = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("MUITO OBRIGADO!");
+            Console.WriteLine($"Alcool: {alcool}\nGasolina: {gasoline}\nDiesel: {diesel}");
+
+        }
     }
 }
