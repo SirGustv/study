44f12e8b76539041f34f0804c29886ee5db196db
Gustavo <gust.bersan@gmail.com>
2022-09-26 18:09:38 -0300

    Adicionando descrição
    Adicionando descrição dos exercícios feitos relacionados a classes, atributos e métodos em seus respectivos códigos.

--- Csharp/POOExercicies/POOExe3/Program.cs
+++ Csharp/POOExercicies/POOExe3/Program.cs
@@ -6,6 +6,9 @@ namespace POOExe3
     class Program
     {
         static void Main(string[] args)
+        // Fazer um programa para ler os valores da largura e altura de um retângulo. Em
+        // seguida, mostrar na tela o valor de sua área, perímetro e diagonal. Usar uma classe
+        // como mostrado no projeto ao lado.
         {
             Console.Clear();
             Rectangle rectangle = new Rectangle();
--- Csharp/POOExercicies/POOExe4/Program.cs
+++ Csharp/POOExercicies/POOExe4/Program.cs
@@ -6,6 +6,12 @@ namespace POOExe4
     class Program
     {
         static void Main(string[] args)
+        // Fazer um programa para ler os dados de um funcionário (nome,
+        // salário bruto e imposto). Em seguida, mostrar os dados do
+        // funcionário (nome e salário líquido). Em seguida, aumentar o salário
+        // do funcionário com base em uma porcentagem dada (somente o
+        // salário bruto é afetado pela porcentagem) e mostrar novamente os
+        // dados do funcionário. Use a classe projetada abaixo.
         {
             Console.Clear();
 
