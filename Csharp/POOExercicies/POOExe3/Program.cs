77a5ee5a352c70037c9a59c6d7e6a7c5560bded3
Gustavo <gust.bersan@gmail.com>
2022-09-26 16:36:17 -0300

    Solucionando exercício
    Resolvendo primeiro exercício de três. Exercício de fixação sobre programação orientada a objetos.

---
+++ Csharp/POOExercicies/POOExe3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace POOExe3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Rectangle rectangle = new Rectangle();
+
+            Console.WriteLine("Entre a largura e a altura do retãngulo:");
+            rectangle.Width = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            rectangle.Height = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("AREA = " + rectangle.Area().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PERÍMETRO = " + rectangle.Perimeter().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("DIAGONAL = " + rectangle.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
