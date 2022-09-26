63d0b8e3ee00f06a6e879959bed3074aa35a40d5
Gustavo <gust.bersan@gmail.com>
2022-09-26 17:21:13 -0300

    Solucionando exercpicio
    Segundo exercício de fixação sobre classes introdutórias com POO resolvido. Obs: As pastas estão com o nome de 'POOExe' mas não necessáriamente se tratam da mesma categoria de exercícios. Do 'POOExe1' ao 'POOExe2' são uma lista, do 'POOExe3' ao 'POOExe5' é outra.

---
+++ Csharp/POOExercicies/POOExe4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace POOExe4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Funcionary funcionary = new Funcionary();
+
+            Console.Write("Nome: ");
+            funcionary.Name = Console.ReadLine();
+            Console.Write("Salário: ");
+            funcionary.GrossSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Imposto: ");
+            funcionary.Tax = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+            Console.WriteLine("Funcionário: " + funcionary);
+
+            Console.WriteLine();
+            Console.Write("Digite a porcentagem para aumentar o salário: ");
+            double percent = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            funcionary.IncreaseSalary(percent);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + funcionary);
+        }
+    }
+}
