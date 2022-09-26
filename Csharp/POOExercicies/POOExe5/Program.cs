f8044a91312b82f66e75a130c1dc313d0a4355ea
Gustavo <gust.bersan@gmail.com>
2022-09-26 18:05:32 -0300

    Solucionando exercícios
    Terceiro exercício de fixação de classes, atributos e métodos resolvido.

---
+++ Csharp/POOExercicies/POOExe5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace POOExe5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Student student = new Student();
+
+            Console.Write("Nome do aluno: ");
+            student.Name = Console.ReadLine();
+            Console.WriteLine("Digite as três notas do aluno:");
+            student.Note1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            student.Note2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            student.Note3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("NOTA FINAL = "
+            + student.Result().ToString("F2", CultureInfo.InvariantCulture));
+            if (student.Aproved())
+                Console.WriteLine("APROVADO");
+            else
+            {
+                Console.WriteLine("REPROVADO");
+                Console.WriteLine("FALTARAM "
+                + student.RemainingNote().ToString("F2", CultureInfo.InvariantCulture)
+                + " PONTOS");
+            }
+
+        }
+    }
+}
