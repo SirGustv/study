bb9d3221f1fff19c414f5eee5593d845823ec38b
Gustavo <gust.bersan@gmail.com>
2022-12-05 22:18:55 -0300

    Adicionando novo exercício
    Atualmente só os aquivos e algumas classes. Este exercício irá abordar Enumerações e Composições.

---
+++ Csharp/POOExercicies/ClientOrder/Entities/OrderItem.cs
@@ -0,0 +1,22 @@
+namespace ClientOrder.Entities
+{
+    public class OrderItem
+    {
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+
+        public OrderItem()
+        {
+        }
+        public OrderItem(int quantity, double price)
+        {
+            Quantity = quantity;
+            Price = price;
+        }
+
+        public double SubTotal()
+        {
+            return Price * Quantity;
+        }
+    }
+}
