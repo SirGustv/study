a74d33ff2606099b2ed0ca5574b22c532889e08b
Gustavo <gust.bersan@gmail.com>
2022-12-06 21:58:43 -0300

    Atualizando Classes
    Adicionando a classe de pedido e adicionando atributos na classe pedido de item.

---
+++ Csharp/POOExercicies/ClientOrder/Entities/Order.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using ClientOrder.Entities;
+using ClientOrder.Entities.Enums;
+
+namespace ClientOrder.Entities
+{
+    public class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+        public Client Client { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+        public Order()
+        {
+        }
+        public Order(DateTime moment, OrderStatus status, Client client)
+        {
+            Moment = moment;
+            Status = status;
+            Client = client;
+        }
+
+        public void AddItem(OrderItem item)
+        {
+            Items.Add(item);
+        }
+        public void RemoeItem(OrderItem item)
+        {
+            Items.Remove(item);
+        }
+        public double Total()
+        {
+            double sum = 0.0;
+            foreach (OrderItem item in Items)
+            {
+                sum += item.SubTotal();
+            }
+            return sum;
+        }
+    }
+}
--- Csharp/POOExercicies/ClientOrder/Entities/OrderItem.cs
+++ Csharp/POOExercicies/ClientOrder/Entities/OrderItem.cs
@@ -4,14 +4,16 @@ namespace ClientOrder.Entities
     {
         public int Quantity { get; set; }
         public double Price { get; set; }
+        public Product Product { get; set; }
 
         public OrderItem()
         {
         }
-        public OrderItem(int quantity, double price)
+        public OrderItem(int quantity, double price, Product product)
         {
             Quantity = quantity;
             Price = price;
+            Product = product;
         }
 
         public double SubTotal()
