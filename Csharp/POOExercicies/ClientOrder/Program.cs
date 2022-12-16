023d23bf84e348c1404e90b3180cbc412d27f34c
Gustavo <gust.bersan@gmail.com>
2022-12-16 12:51:25 -0300

    Adicionando interação com usuário
    O sistema de pedidos está completamente funcional e interativo com o usuário.

--- Csharp/POOExercicies/ClientOrder/Program.cs
+++ Csharp/POOExercicies/ClientOrder/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Globalization;
+using ClientOrder.Entities;
+using ClientOrder.Entities.Enums;
 
 namespace ClientOrder
 {
@@ -6,7 +9,45 @@ namespace ClientOrder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Clear();
+
+            Console.WriteLine("Enter client data:");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("E-Mail: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth Date (DD/MM/YYYY): ");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Enter order data: ");
+            Console.Write("Status: ");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+
+            Client client = new Client(name, email, birthDate);
+            Order order = new Order(DateTime.Now, status, client);
+
+            Console.Write("How many items to this order? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Enter #{i} item data:");
+                Console.Write("Product name: ");
+                string productName = Console.ReadLine();
+                Console.Write("Product price: ");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Product product = new Product(productName, price);
+
+                Console.Write("Quantity: ");
+                int qnt = int.Parse(Console.ReadLine());
+
+                OrderItem orderItem = new OrderItem(qnt, price, product);
+
+                order.AddItem(orderItem);
+            }
+
+            Console.WriteLine($"\nORDER SUMARY:");
+            Console.WriteLine(order);
         }
     }
 }
